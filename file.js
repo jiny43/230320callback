e5b23d948502c60ffe78e0af63e983fdcc4b9a80
jieun <je_eun2735@naver.com>
2023-03-20 12:00:08 +0900

    writeFileSync()

---
+++ file.js
@@ -0,0 +1,10 @@
+import fs from 'fs';
+let name = "jingjieun";
+// let date = new Date();
+// let fileName = date.getSeconds() + name;
+
+let year = "2023";
+
+fs.writeFileSync("./"+ name+".txt",year);
+//!파일을 동기방식으로 쓰다.
+//?test.txt 에 name 을 새겨서 만들었다.
